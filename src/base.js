31409dbce1a065213871141ed7a5972764af6e13
Xiang Yong <ivan.xyong@gmail.com>
2018-09-19 19:27:55 +0800

    fix canWebp bug

--- src/base.js
+++ src/base.js
@@ -1,10 +1,7 @@
 const VueImg = Object.create(null)
 
 // Check webP support
-VueImg.canWebp = false
-const img = new Image()
-img.onload = () => { VueImg.canWebp = true }
-img.src = 'data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAsAAAABBxAREYiI/gcAAABWUDggGAAAADABAJ0BKgEAAQABABwlpAADcAD+/gbQAA=='
+VueImg.canWebp = !![].map && document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') === 0;
 
 // Default cdn prefix
 const protocol = location.protocol === 'https:' ? 'https://' : 'http://'
